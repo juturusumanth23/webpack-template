2c21e54cb5c0175b1a161483cda09595c826630b
juturusumanth23 <48328516+juturusumanth23@users.noreply.github.com>
2025-08-05 20:35:47 +0530

    Delete src/index.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-import odinImage from "./odin.png";
-   
-const image = document.createElement("img");
-image.src = odinImage;
-   
-document.body.appendChild(image);
-
-console.log("Hello Todo-list")
